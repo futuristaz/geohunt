1a19e73d83f09d93b245ebe66335dc7db1de6b53
Lukas Zujevas <zujevasl@gmail.com>
2025-09-24 17:14:16 +0300

    removed redundant code and renamed endpoints

--- psi25-project/Program.cs
+++ psi25-project/Program.cs
@@ -4,7 +4,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<EventsRepository>();
 builder.Services.AddHttpClient<GoogleMapsService>();
 
 var app = builder.Build();
@@ -15,7 +14,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapGet("/maps/distance", async (string address1, string address2, GoogleMapsService mapsService) =>
+app.MapGet("api/geocoding/distance", async (string address1, string address2, GoogleMapsService mapsService) =>
 {
 try
 {
@@ -33,7 +32,7 @@ try
     }
 });
 
-app.MapGet("/maps", async (string address, GoogleMapsService mapsService) =>
+app.MapGet("api/geocoding/address", async (string address, GoogleMapsService mapsService) =>
 {
     try 
     {
@@ -46,67 +45,4 @@ app.MapGet("/maps", async (string address, GoogleMapsService mapsService) =>
     }
 });
 
-app.MapGet("/", async () =>
-{
-    await Task.Delay(1000);
-    return "Hello, tester (delayed)";
-});
-
-app.MapGet("/problem", () => {
-    return TypedResults.StatusCode(200);
-});
-
-app.MapGet("/greet/{name?}", (string name = "Guest", int age = 99) =>
-{
-    var text = $"Hello, {name}! You are {age} years old!";
-    return text;
-});
-
-app.MapGet("/test1", () => {
-    return new ComplexType
-    {
-        Id = 1,
-        Name = "Name",
-    };
-});
-
-app.MapPost("/testingpost", (ComplexType complexType) =>
-{
-    return $"id: {complexType.Id}, name: {complexType.Name}";
-});
-
-app.MapGet("/events", (EventsRepository repository) => repository.Get());
-
-app.MapDelete("/", () => "This is a DELETE request!");
-app.MapPut("/", () => "This is a PUT request!");
-app.MapPatch("/", () => "This is a PATCH request!");
-
 app.Run();
-
-class ComplexType
-{
-    public required int Id { get; init; }
-    public required string Name { get; init; }
-}
-
-class EventsRepository
-{
-    public void Save(Event eventModel)
-    {
-        // Save the event
-    }
-
-    public Event[] Get()
-    {
-        return [
-            new Event { Id = 1, Name = "Event 1" },
-            new Event { Id = 2, Name = "Event 2" }
-        ];
-    }
-}
-
-class Event 
-{
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
