a3e86883730928012923410b6215eca2001b84a9
Lukas Zujevas <zujevasl@gmail.com>
2025-10-15 12:59:24 +0300

    Refactor DistanceController to remove unnecessary parameter in CalculateHaversineDistance; enhance error handling in streetView.js for game initialization and score updating

--- psi25-project/wwwroot/Scripts/streetView.js
+++ psi25-project/wwwroot/Scripts/streetView.js
@@ -17,6 +17,14 @@ async function initMap() {
     }
   );
 
+  const responseLocation = await postLocation({ latitude: lat, longitude: lng, panoId: data.panoID });
+  const responseGame = await postGame({UserId: "f5f925f5-4345-474e-a068-5169ab8bcb15"}); // TODO get actual user id
+
+  if (!responseLocation || !responseGame) {
+    showMessage('Error initializing game. Please try again later.', 'error');
+    return;
+  }
+
   const miniMap = new google.maps.Map(document.getElementById('mini-map'), {
     center: { lat: 0.0, lng: 0.0},
     zoom: 1,
@@ -67,7 +75,7 @@ async function initMap() {
     sessionStorage.setItem('guessData', JSON.stringify(guessData));
     const distance = await fetchDistance(guessData);
     sessionStorage.setItem('distance', distance);
-    console.log('Distance:', distance);
+    const updatedGame = await updateScore(responseGame.id, Math.round(Number(distance))); // TODO: replace with actual score
 
     window.location.href = '/result.html';
   })
@@ -98,6 +106,57 @@ async function fetchDistance(data) {
     return distance;
   } catch (error) {
     console.error('Error fetching distance:', error);
+    return null;
+  }
+}
+
+async function postLocation(data) {
+  try {
+    const response = await fetch("api/Locations", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+    const location = await response.json();
+    return location;
+  } catch (error) {
+    console.error('Error posting location:', error);
+    return null;
+  }
+}
+
+async function postGame(data) {
+  try {
+    const response = await fetch("api/Game", {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+  });
+    const game = await response.json();
+    return game;
+  } catch (error) {
+    console.error('Error posting game:', error);
+    return null;
+  }
+}
+
+async function updateScore(gameId, score) {
+  try {
+    const response = await fetch(`api/Game/${gameId}/score`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(score)
+    });
+    const updatedGame = await response.json();
+    return updatedGame;
+  } catch (error) {
+    console.error('Error updating score:', error);
     return null;
   }
 }
