8f1d97e3c932ba87a1458ec35c654c9275535f0b
mazvydiekas <mazpeles@gmail.com>
2025-11-13 11:19:33 +0200

    Small fix

--- psi25-project/Repositories/Interfaces/IUserRepository.cs
+++ psi25-project/Repositories/Interfaces/IUserRepository.cs
@@ -1,26 +1,10 @@
-using Microsoft.EntityFrameworkCore;
-using psi25_project.Data;
 using psi25_project.Models;
 
 namespace psi25_project.Repositories
 {
-    public class UserRepository : IUserRepository
+    public interface IUserRepository
     {
-        private readonly GeoHuntContext _context;
-
-        public UserRepository(GeoHuntContext context)
-        {
-            _context = context;
-        }
-
-        public async Task<IEnumerable<ApplicationUser>> GetAllAsync()
-        {
-            return await _context.Users.AsNoTracking().ToListAsync();
-        }
-
-        public async Task<ApplicationUser?> GetByIdAsync(Guid id)
-        {
-            return await _context.Users.AsNoTracking().FirstOrDefaultAsync(u => u.Id == id);
-        }
+        Task<IEnumerable<ApplicationUser>> GetAllAsync();
+        Task<ApplicationUser?> GetByIdAsync(Guid id);
     }
 }
