2eac66db710ddcb37b7a42c283268cc49257cdb7
R0ckguy <tankrokas@gmail.com>
2025-11-17 13:51:05 +0200

    Moved Serilog config

--- psi25-project/Program.cs
+++ psi25-project/Program.cs
@@ -16,15 +16,7 @@ using Polly;
 using Polly.Extensions.Http;
 
 // Configure Serilog
-Log.Logger = new LoggerConfiguration()
-    .WriteTo.Console()
-    .WriteTo.File(
-        path: "logs/geohunt-.log",
-        rollingInterval: RollingInterval.Day,
-        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
-        retainedFileCountLimit: 30)
-    .Enrich.FromLogContext()
-    .CreateLogger();
+Log.Logger = LoggingConfiguration.CreateLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 
